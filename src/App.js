2c67778068a5124fb4169c73965d57b29b4733cf
Quan Thien Bao Tran <tranthienbaoquan@gmail.com>
2021-05-17 05:47:19 +0000

    add bot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = (props) => {
+  const [telegramBotToken, setTelegramBotToken] = useState('');
+  const [telegramChatIDs, setTelegramChatIDs] = useState([]);
+  const [message, setMessage] = useState('');
+
+  const handleTelegramBotTokenChange = (e) => {
+    setTelegramBotToken(e.target.value);
+  }
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  }
+
+  const fetchChatIDs = async () => {
+    if (telegramBotToken) {
+      setTelegramChatIDs([]);
+      await axios
+        .get(
+          `https://api.telegram.org/bot${telegramBotToken}/getUpdates`
+        )
+        .then(function (response) {
+          const data = response.data;
+          if (data.result.length === 0) {
+            alert('No Chat ID found!');
+            return;
+          }
+          const IDs = [];
+          for (let id of data.result) {
+            if (id.my_chat_member) {
+              IDs.push({
+                id: id.my_chat_member.chat.id,
+                title: id.my_chat_member.chat.title
+              });
+            }
+          }
+          setTelegramChatIDs(IDs);
+        })
+        .catch(function (error) {
+          alert('Error!')
+        });
+    } else {
+      alert('Telegram Bot Token not found!');
+    }
+  };
+
+  const sendMessage = async () => {
+    if (message && telegramChatIDs && telegramChatIDs.length > 0) {
+      for (let chat of telegramChatIDs) {
+        await axios
+          .get(
+            `https://api.telegram.org/bot${telegramBotToken}/sendMessage`,
+            {
+              params: {
+                chat_id: chat.id,
+                text: message,
+              },
+            },
+          )
+          .then(function (response) {
+          })
+          .catch(function (error) {
+          });
+      }
+      alert('Done!');
+    } else {
+      alert('Cannot send!');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h2>TokenPlay Telegram Bot</h2>
+      <div>
+        <label>Telegram Bot Token: </label>
+        <input
+          value={telegramBotToken}
+          onChange={handleTelegramBotTokenChange}
+        />
+        <button
+          onClick={fetchChatIDs}
         >
-          Learn React
-        </a>
-      </header>
+          Fetch Chat IDs
+        </button>
+      </div>
+      <br/>
+      <textarea
+        readOnly
+        rows="5"
+        cols="40"
+        value={telegramChatIDs.map(id => id.title).join('\n')}
+      />
+      <br/>
+      <label>Message: </label>
+      <br/>
+      <textarea
+        rows="5"
+        cols="40"
+        value={message}
+        onChange={handleMessageChange}
+      />
+      <br/>
+      <button
+        onClick={sendMessage}
+      >
+        SEND
+      </button>
     </div>
   );
 }
